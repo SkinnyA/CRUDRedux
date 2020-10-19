5147e15b4bdd445d1668056c7cbd3f72fc7602fa
unknown <aarguelles05@gmail.com>
2020-10-19 13:55:14 +0200

    Deleting producuts from state and API

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -96,11 +96,23 @@ const descargaProductosError = () => ({
 // selecciona y elimina el producto
 export function borrarProductoAction(id) {
     return async (dispatch) => {
-        dispatch(obtenerProductoEliminar(id))
+        dispatch(obtenerProductoEliminar(id));
+
+        try {
+            await clienteAxios.delete(`/productos/${id}`);
+            dispatch(eliminarProductoExito());
+        } catch (error) {
+            
+        }
+
     }
 }
 
 const obtenerProductoEliminar = id => ({
     type: OBTENER_PRODUCTO_ELIMINAR,
     payload: id
+});
+
+const eliminarProductoExito = () => ({
+    type: PRODUCTO_ELIMINADO_EXITO
 });
