30bea60fcefa9e88f9c1b8edeec57653c37478b2
unknown <aarguelles05@gmail.com>
2020-10-19 13:01:03 +0200

    Passing data from form to action

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -5,8 +5,8 @@ import {
 } from '../types';
 
 // crear nuevos productos
-export function crearNuevoProductoAction(){
+export function crearNuevoProductoAction(producto){
     return () => {
-        console.log('Desde action')
+        console.log(producto)
     }
 }
