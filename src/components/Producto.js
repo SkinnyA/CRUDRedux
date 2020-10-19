0fa517577f1257331ba25b7722068e13f251496e
unknown <aarguelles05@gmail.com>
2020-10-19 14:01:27 +0200

    Adding sweet alert when clicking delete button

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 // redux
 import {useDispatch} from 'react-redux';
@@ -14,9 +15,23 @@ const Producto = ({producto}) => {
     // confirmar si desea eliminarlo
     const confirmarEliminarProducto = id => {
         // preguntar al usuario
+        Swal.fire({
+            title: 'Estas seguro?',
+            text: "Un producto que se elimina no se puede recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar!',
+            cancelButtonText: 'Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                // pasarlo al action
+                dispatch(borrarProductoAction(id));
+            }
+          })
 
-        // pasarlo al action
-        dispatch(borrarProductoAction(id));
+       
     }
 
     return (  
